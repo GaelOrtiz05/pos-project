dd3a5b6f4146dc1413f985d89a067c4e8b707a99
GaelOrtiz05 <gaelortiz747@gmail.com>
2026-03-23 13:39:00 -0500

    removed comments

--- src/cpp/database.hpp
+++ src/cpp/database.hpp
@@ -2,8 +2,6 @@
 #define DATABASE_HPP
 #include "SQLiteCpp/Database.h"
 #include "SQLiteCpp/SQLiteCpp.h"
-#include <iomanip>
-#include <iostream>
 
 struct Ingredient {
   int id; // in my head this would be used by sqlite?
@@ -54,37 +52,3 @@ public:
 };
 
 #endif
-
-//
-// void display() {
-//
-//   std::vector<Item> items = pos_db.getItems();
-//
-//   int totalWidth = 31;
-//   int colWidth = 15;
-//   if (items.empty() == true) {
-//     std::cout << "\nMenu is empty\n";
-//     return;
-//   } else {
-//
-//     std::cout << std::setfill('-') << std::setw(31) << "\n";
-//     std::cout << std::setfill(' ') << std::setw(25) << std::right
-//               << "Inventory From DB\n";
-//     std::cout << std::setfill('-') << std::setw(totalWidth) << "\n";
-//     std::cout << std::setfill(' ');
-//     std::cout << std::setw(colWidth) << std::left << "Name" << "|";
-//     std::cout << std::setw(colWidth) << std::right << "Price\n";
-//     std::cout << std::setfill('-') << std::setw(totalWidth) << "\n";
-//     std::cout << std::setfill(' ');
-//
-//     for (const auto &item : items) {
-//       std::cout << std::setw(colWidth) << std::left << item.name << "|";
-//       std::cout << std::setw(colWidth) << std::right << item.price << "\n";
-//     }
-//
-//     // display ingredient stock too?
-//
-//     std::cout << std::setfill('-') << std::setw(31) << "\n";
-//     std::cout << std::setfill(' ');
-//   }
-// }
